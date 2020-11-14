393780df11e7dc4fd25656844897eb924eb6e6d2
Samantha Sainsbury <samanthasains@gmail.com>
2020-11-14 17:59:54 -0600

    Updated ED and schema

--- sql-challenge-schema.sql
+++ sql-challenge-schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE employees (
     sex CHAR(1),
     hire_date DATE,
     PRIMARY KEY (emp_no),
-    FOREIGN KEY (emp_title_id) REFERENCES Titles(title_id)
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 --Create Departments table, set dept_no as primary key
@@ -29,25 +29,25 @@ CREATE TABLE departments (
 --Create Department Manager table, set dept_no as primary key and dept_no and emp_no as foreign keys
 CREATE TABLE department_manager (
     dept_no VARCHAR(30),
-    emp_no VARCHAR(30),
+    emp_no VARCHAR(10),
     PRIMARY KEY (dept_no, emp_no),
-    FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
-    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 --Create Department Employees table, set emp_no as primary key and emp_no and dept_no as foreign keys
 CREATE TABLE dept_employees (
-    emp_no VARCHAR(30),
+    emp_no VARCHAR(10),
     dept_no VARCHAR(30),
     PRIMARY KEY (emp_no),
-    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
-    FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 --Create Salaries table, set emp_no as primary key and emp_no as foreign key
 CREATE TABLE salaries (
-    emp_no VARCHAR(30),
+    emp_no VARCHAR(10),
     salary INT,
     PRIMARY KEY (emp_no),
-    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
